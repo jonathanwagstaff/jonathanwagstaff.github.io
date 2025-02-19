2ab00fbfb6af86decca563ab502f9bfb9a2db922
jonathanwagstaff <warriorhero4truth@gmail.com>
2025-02-18 20:05:07 -0700

    Add files via upload

---
+++ Doge's_Academy_for_Superheroes/Doge's_Academy_for_Superheroes/Doge's_Academy_for_Superheroes/Javascript/landing_page.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let move = prompt("You are on the landing page. Would you like to move on to the home page?:\nyes / no");
+
+if(move == "yes")
+{
+    alert("For now, just pretend you are being transported to the main page.");
+} else
+{
+    alert("Okay, thank you for viewing!")
+}
